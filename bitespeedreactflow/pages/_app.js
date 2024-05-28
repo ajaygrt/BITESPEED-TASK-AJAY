691503a871bc1d3e74f498e734d76f427a3776bd
AjayFCS <ajay.lakshman41@gmail.com>
2024-05-29 02:00:06 +0530

    fixes

--- bitespeedreactflow/pages/_app.js
+++ bitespeedreactflow/pages/_app.js
@@ -1,9 +1,15 @@
 // pages/_app.js
-import 'reactflow/dist/style.css';
-import '../styles/globals.css'; // Ensure you have this file or update the path accordingly
+import "reactflow/dist/style.css";
+import "../styles/globals.css"; // Ensure you have this file or update the path accordingly
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Component {...pageProps} />
+    </DndProvider>
+  );
 }
 
 export default MyApp;
--- bitespeedreactflow/pages/index.js
+++ bitespeedreactflow/pages/index.js
@@ -1,20 +1,10 @@
-import React from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import ChatbotFlowBuilder from "./chatbotflowbuilder";
+import ChatbotFlowBuilder from './chatbotflowbuilder';
 
 // Wrap your component tree with DndProvider
 const App = () => {
   return (
-    <React.StrictMode>
-      <DndProvider backend={HTML5Backend}>
-        <ChatbotFlowBuilder />
-      </DndProvider>
-    </React.StrictMode>
+      <ChatbotFlowBuilder />
   );
 };
 
-// Use createRoot to render the App component
-const root = document.getElementById("root");
-const rootElement = React.createRoot(root);
-rootElement.render(<App />);
+export default App;
